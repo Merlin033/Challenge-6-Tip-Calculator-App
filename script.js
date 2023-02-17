42f1aa291374d75a884fc2608fc76160585d3b0d
Merlin033 <barnabedebocq@live.fr>
2023-02-17 23:22:21 +0100

    Added ergonomics to input fields

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const numberInput = document.querySelector('input[name="number"]');
 const tipAmountOutput = document.querySelector("#tipAmount");
 const totalOutput = document.querySelector("#total");
 const resetButton = document.querySelector(".reset button");
+const billInput = document.getElementById("billInput");
+const bill = document.getElementById("bill");
+const people = document.getElementById("people");
+const peopleInput = document.getElementById("peopleInput");
 
 let price = 0;
 let tip = 0;
@@ -18,17 +22,42 @@ priceInput.addEventListener("input", () => {
   calculate();
 });
 
+// Listening keypress events on input field
+priceInput.addEventListener("input", (event) => {
+  let input = event.target.value;
+
+  // Vérification 1: limiter la longueur totale à 8 caractères
+  if (input.length > 8) {
+    input = input.slice(0, 8);
+  }
+
+  // Vérification 2: n'autoriser que les chiffres et un seul point décimal
+  const regex = /^[0-9]+(\.[0-9]{0,2})?$/;
+  if (!regex.test(input)) {
+    input = input.slice(0, -1);
+  }
+
+  // Vérification 3: s'assurer que la valeur est entre 0 et 9999.99
+  const value = parseFloat(input);
+  if (isNaN(value) || value < 0 || value > 9999.99) {
+    input = "";
+  }
+
+  // Mettre à jour la valeur affichée dans le champ
+  event.target.value = input;
+});
+
 // écouter les événements click sur les boutons de pourboire
 tipButtons.forEach((button) => {
   button.addEventListener("click", () => {
     tipButtons.forEach((btn) => btn.classList.remove("active"));
     button.classList.add("active");
-    if (button.textContent === "Custom") {
+    if (button.textContent >= 1 && button.textContent <= 100) {
       customTipInput.classList.add("active");
       customTipInput.focus();
     } else {
       customTipInput.classList.remove("active");
-      tip = parseFloat(button.textContent) / 100;
+      tip = parseFloat(button.textContent) / 100 || 0;
       calculate();
     }
   });
@@ -48,15 +77,39 @@ numberInput.addEventListener("input", () => {
 
 // écouter les événements click sur le bouton de réinitialisation
 resetButton.addEventListener("click", () => {
-  priceInput.value = "";
-  tipButtons[2].click();
-  numberInput.value = "";
+  priceInput.value = "0";
+  customTipInput.value = "0";
+  numberInput.value = "1";
+  price = 0;
+  numberOfPeople = 1;
+  calculate();
 });
 
 // fonction de calcul des résultats
 function calculate() {
   const tipAmount = (price * tip) / numberOfPeople;
-  const total = (price * (1 + tip)) / numberOfPeople;
+  const total = price + tipAmount;
+
   tipAmountOutput.textContent = `$${tipAmount.toFixed(2)}`;
   totalOutput.textContent = `$${total.toFixed(2)}`;
+
+  billInput.style.width = (billInput.value.length + 1) * 15 + "px";
+  peopleInput.style.width = (peopleInput.value.length + 1) * 15 + "px";
 }
+
+// Bill and numberofpeople focus and style
+bill.addEventListener("click", () => {
+  billInput.focus();
+});
+people.addEventListener("click", () => {
+  peopleInput.focus();
+});
+
+resizeBillInput = () => {
+  billInput.style.width = (billInput.value.length + 1) * 15 + "px";
+};
+resizePeopleInput = () => {
+  peopleInput.style.width = (peopleInput.value.length + 1) * 15 + "px";
+};
+
+calculate();
